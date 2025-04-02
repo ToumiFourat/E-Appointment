16f80ff07122141d04e2311737db6d4ca0dfcf1a
ToumiFourat <fourattoumi021@gmail.com>
2025-04-03 00:47:39 +0100

    Guess who’s back?

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,11 +3,7 @@ import {v2 as cloudinary} from 'cloudinary'
 const connectCloudinary = async () => {
     cloudinary.config({
         cloud_name:process.env.CLOUDINARY_NAME,
-<<<<<<< HEAD
-        api_key:process.env.CLOUDINARY_API_NKEY,
-=======
         api_key:process.env.CLOUDINARY_API_KEY,
->>>>>>> 663dfd5 (Guess who’s back?)
         api_secret:process.env.CLOUDINARY_SECRET_KEY,
     })
 }
--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -5,17 +5,10 @@ import jwt from 'jsonwebtoken'
 const authAdmin = async (req,res,next) => {
     try {
         const {atoken} = req.headers
-<<<<<<< HEAD
-        if (!token) {
-            return res.json({success:false,message:'Non autorisé. Veuillez vous reconnecter'})
-        }
-        const token_decode = jwt.verify(atoken,process.JWT_SECRET)
-=======
         if (!atoken) {
             return res.json({success:false,message:'Non autorisé. Veuillez vous reconnecter'})
         }
         const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
->>>>>>> 663dfd5 (Guess who’s back?)
 
         if (token_decode !==process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD  ) {
             return res.json({success:false,message:'Non autorisé. Veuillez vous reconnecter'})
