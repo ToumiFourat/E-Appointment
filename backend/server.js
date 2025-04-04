845cf4313ac0e0d0e93b9f3fec665a996f4a5251
ToumiFourat <fourattoumi021@gmail.com>
2025-04-04 23:16:07 +0100

    Initializing Admin Dashboard frontend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,7 @@ import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoutes.js';
 // Configuration de l'application
 dotenv.config();
-console.log("CLOUDINARY_API_KEY:", process.env.CLOUDINARY_API_KEY);
-console.log("CLOUDINARY_NAME:", process.env.CLOUDINARY_NAME);
-console.log("CLOUDINARY_SECRET_KEY:", process.env.CLOUDINARY_SECRET_KEY);
+
 
 const app = express();
 const port = process.env.PORT || 4000;
